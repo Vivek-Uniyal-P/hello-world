5d5616ba9d275ec164f0bcba7f4d9faa744c082c
Vivek Uniyal <vivek.un22@gmail.com>
2023-08-20 11:48:58 +0530

    Added Help function usage.

--- Sample.py
+++ Sample.py
@@ -3,5 +3,13 @@ print("This is a sample Python file.")
 mylist = [x for x in range(1,101) if x%2 == 0]
 shuffle(mylist)
 
+help(mylist.insert) # help function can be used to see the documentation about a function.
+'''
+Help on built-in function insert:
+
+insert(index, object, /) method of builtins.list instance
+    Insert object before index.
+'''
+
 
 
